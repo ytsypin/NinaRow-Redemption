aa08d6304c46accead33c999ba08a9877634376d
Yuli <ytsypin@gmail.com>
2018-09-03 15:47:49 +0300

    Ground work for leave button

--- NinaGameModule/src/regularGame/RegularGame.java
+++ NinaGameModule/src/regularGame/RegularGame.java
@@ -434,7 +434,12 @@ public class RegularGame{
     public void cascadeTiles(int row, int col) {
         boolean winnersFound = false;
 
-        while((row > 0) && gameBoard.getTileSymbol(row-1, col) != NinaBoard.getEmptyTile(){
+        while((row > 0) && gameBoard.getTileSymbol(row-1, col) != NinaBoard.getEmptyTile()){
+            int currentTile = gameBoard.getTileSymbol(row-1, col);
+            gameBoard.dropTile(row-1, col);
+
+            checkForWinner(row, col, currentTile);
+
 
         }
     }
